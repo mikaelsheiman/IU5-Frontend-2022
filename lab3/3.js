5e273e1cfea5b7147b6798036eb5aa392012935c
mikaelsheiman <63838221+mikaelsheiman@users.noreply.github.com>
2022-03-13 22:39:11 +0300

    Лаб3

--- lab3/3.js
+++ lab3/3.js
@@ -7,8 +7,19 @@
  * [1, 2, 3, 'ddd', {min: 1}, 22, false], 2 -> [2, 4, 6, 'ddd', {min: 1}, 44, false]
  */
 
-function multiplyArray(arr, n) {
-    // code here
+ function multiplyArray(arr, n) {
+
+    let n_arr = arr;
+
+    for (let i = 0; i < n_arr.length; ++i) {
+
+        if (typeof(n_arr[i]) == 'number') {
+
+            n_arr[i] *= n;
+        }
+    }
+    
+    return n_arr;
 }
 
 module.exports = multiplyArray;
